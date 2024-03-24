4c7a955216429e00e255aa95e1cada5d19e495d1
Bogdan Kovalov <kovalov.univ@gmail.com>
2024-03-24 13:25:47 +0200

    Textures fixed

--- Solar System Simulation/Main.cpp
+++ Solar System Simulation/Main.cpp
@@ -9,6 +9,10 @@
 
 #include <iostream>
 
+void ProcessInput(GLFWwindow* Window, int Key, int Scancode, int Action, int Mods);
+
+float MixCoef = 0.2f;
+
 GLFWwindow* InitializeSystem()
 {
     int Success = glfwInit();
@@ -36,6 +40,8 @@ GLFWwindow* InitializeSystem()
     glfwGetFramebufferSize(Window, &Width, &Height);
     glViewport(0, 0, Width, Height);
 
+    glfwSetKeyCallback(Window, ProcessInput);
+
     return Window;
 }
 
@@ -57,8 +63,8 @@ GLuint GenerateTexture(const char* FileName)
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, Width, Height, 0, GL_RGB, GL_UNSIGNED_BYTE, Data);
     glGenerateMipmap(GL_TEXTURE_2D);
@@ -68,152 +74,47 @@ GLuint GenerateTexture(const char* FileName)
     return Texture;
 }
 
-void Try() 
+GLuint GenerateTextureWithAlpha(const char* FileName)
 {
-    auto Window = InitializeSystem();
-    Shader MainShader("Shaders/Vertex.v", "Shaders/Fragment.frag");
-    float vertices[] = {
-        // координаты        // цвета            // текстурные координаты
-        0.5f, 0.5f, 0.0f, 1.0f, 1.0f,    // верхняя правая вершина
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,   // нижняя правая вершина
-        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f,  // нижняя левая вершина
-        -0.5f, 0.5f, 0.0f,  0.0f, 1.0f    // верхняя левая вершина
-    };
-    unsigned int indices[] = {
-        0, 1, 3,  // первый треугольник
-        1, 2, 3   // второй треугольник
-    };
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+    int Width = 0, Height = 0, NrChannels = 0;
+    unsigned char* Data = stbi_load(FileName, &Width, &Height, &NrChannels, 0);
 
-    glBindVertexArray(VAO);
+    if (!Data)
+    {
+        std::cout << "Failed to load image" << std::endl;
+        stbi_image_free(Data);
+        return 0;
+    }
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    GLuint Texture;
+    glGenTextures(1, &Texture);
+    glBindTexture(GL_TEXTURE_2D, Texture);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // Координатные атрибуты
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // Атрибуты текстурных координат
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // Загрузка и создание текстуры
-    unsigned int texture1, texture2;
-    // Текстура №1 - Деревянный ящик
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    // установка параметров наложения текстуры
-    glTexParameteri(
-        GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);  // установка метода наложения текстуры GL_REPEAT (стандартный метод наложения)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    // Установка параметров фильтрации текстуры
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    // Загрузка изображения, создание текстуры и генерирование мипмап-уровней
-    int width, height, nrChannels;
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, Width, Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, Data);
+    glGenerateMipmap(GL_TEXTURE_2D);
 
-    unsigned char* data = stbi_load("Container.jpg", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
+    stbi_image_free(Data);
 
-    // Текстура №2 - Смайлик
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-
-    // Установка параметров наложения текстуры
-    glTexParameteri(
-        GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);  // установка метода наложения текстуры GL_REPEAT (стандартный метод наложения)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    // Установка параметров фильтрации текстуры
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    // Загрузка изображения, создание текстуры и генерирование мипмап-уровней
-    data = stbi_load("Face.png", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        // Файл awesomeface.png имеет альфа-канал (прозрачность), поэтому необходимо использовать пераметр GL_RGBA
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-
-    // Указываем OpenGL какой сэмплер к какому текстурному блоку принадлежит (это нужно сделать единожды)
-    MainShader.Use();  // не забудьте активировать шейдер перед настройкой uniform-переменных!
-
-    // Устанавливаем вручную…
-    glUniform1i(glGetUniformLocation(MainShader.Program, "Texture1"), 0);
-    // …или с помощью шейдерного класса
-    glUniform1i(glGetUniformLocation(MainShader.Program, "Texture2"), 1);
-
-    // Цикл рендеринга
-    while (!glfwWindowShouldClose(Window))
-    {
-
-        // Рендеринг
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Привязка текстур к соответствующим текстурным юнитам
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-
-        // Рендеринг ящика
-        MainShader.Use();
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        // glfw: обмен содержимым front- и back- буферов. Отслеживание событий ввода/вывода (была ли нажата/отпущена кнопка, перемещен
-        // курсор мыши и т.п.)
-        glfwSwapBuffers(Window);
-        glfwPollEvents();
-    }
-
-    // Опционально: освобождаем все ресурсы, как только они выполнили свое предназначение
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+    return Texture;
 }
 
 int main()
 {
-    Try();
-    return 0;
     auto Window = InitializeSystem();
 
     float Vertices[] = {
-        // координаты        // цвета            // текстурные координаты
         0.5f, 0.5f, 0.0f, 1.0f, 1.0f,    // верхняя правая
         0.5f, -0.5f, 0.0f, 1.0f, 0.0f,   // нижняя правая
         -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,  // нижняя левая
-        -0.5f, 0.5f, 0.0f,  0.0f, 1.0f    // верхняя левая
+        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f    // верхняя левая
     };
 
-     unsigned int indices[] = {
+    unsigned int indices[] = {
         0, 1, 3,  // первый треугольник
         1, 2, 3   // второй треугольник
     };
@@ -244,7 +145,7 @@ int main()
     Shader MainShader("Shaders/Vertex.v", "Shaders/Fragment.frag");
 
     GLuint ContainerTexture = GenerateTexture("Container.jpg");
-    GLuint FaceTexture = GenerateTexture("Face.png");
+    GLuint FaceTexture = GenerateTextureWithAlpha("Face.png");
 
     MainShader.Use();
     glUniform1i(glGetUniformLocation(MainShader.Program, "Texture1"), 0);
@@ -252,16 +153,19 @@ int main()
 
     while (!glfwWindowShouldClose(Window))
     {
+        glfwPollEvents();
+
         glClearColor(0.3f, 0.2f, 0.4f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        MainShader.Use();
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, ContainerTexture);
 
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, FaceTexture);
+        glUniform1f(glGetUniformLocation(MainShader.Program, "MixCoef"), MixCoef);
+
+        MainShader.Use();
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
@@ -274,4 +178,20 @@ int main()
 
     glfwTerminate();
     return 0;
+}
+
+void ProcessInput(GLFWwindow* Window, int Key, int Scancode, int Action, int Mods)
+{
+    if (glfwGetKey(Window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        MixCoef += 0.1f;  // измените это значение соответствующим образом (может быть слишком медленным или слишком быстрым, в
+                            // зависимости от вашего системного оборудования)
+        if (MixCoef >= 1.0f) MixCoef = 1.0f;
+    }
+    if (glfwGetKey(Window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        MixCoef -= 0.1f;  // измените это значение соответствующим образом (может быть слишком медленным или слишком быстрым, в
+                            // зависимости от вашего системного оборудования)
+        if (MixCoef <= 0.0f) MixCoef = 0.0f;
+    }
 }
--- Solar System Simulation/Shaders/Fragment.frag
+++ Solar System Simulation/Shaders/Fragment.frag
@@ -5,11 +5,12 @@ in vec2 textureCoord;
 
 uniform sampler2D Texture1;
 uniform sampler2D Texture2;
+uniform float MixCoef;
 
 out vec4 color;
 
 
 void main() 
 {
-	color = mix(texture(Texture1, textureCoord), texture(Texture2, textureCoord), 1.0);
+	color = mix(texture(Texture1, textureCoord), texture(Texture2, vec2(textureCoord.x, textureCoord.y)), MixCoef);
 }
